962e330ca2fc4b1289c52f2aa7bf9caad8cc3361
Mojithmj <mojithmj@gmail.com>
2024-04-09 10:47:12 +0530

    commited

--- src/Student/Swishlist.js
+++ src/Student/Swishlist.js
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
 
 function Swishlist() {
+  const [book, setBook]=useState([])
+  const getborrow=()=>{
+    axios.get('http://localhost:4000/findwishlist').then((response)=>{
+      setBook(response.data.data)
+      console.log(response);
+    })
+    .catch((err)=>{
+      setBook(err)
+    })
+  }
+
+  useEffect(() => {
+    getborrow()
+  }, [])
+  
   return (
     <div>
-<div class="card" style={{width: '18rem'}}>
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+
+
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">BOOK ID</th>
+      <th scope="col">STUDENT ID</th>
+      <th scope="col">DATE BORROWED</th>
+    </tr>
+  </thead>
+  
+    {book.map((value,index)=>(
+
+<tbody>
+    <tr>
+      <th scope="row">{index+1}</th>
+      <td>{value.bookid}</td>
+      <td>{value.studid}</td>
+      <td>{value.Date}</td>
+    </tr>
+    </tbody>
+     ))}
+
+</table>
+
 
     </div>
   )
